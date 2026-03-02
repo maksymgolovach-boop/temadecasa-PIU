c685df357556aac09576b17635f282a8e4309d76
maksymgolovach-boop <maksym.holovach@student.usv.ro>
2026-03-02 19:14:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculsalariu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ore_lucrate = 0;
+            double tarif_ora;
+            double salariu;
+
+            Console.Write("Introduceti tariful pe ora [RON]: ");
+            tarif_ora = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduceti numarul de ore lucrate: ");
+            ore_lucrate = Convert.ToInt32(Console.ReadLine());
+
+            salariu = tarif_ora * (double)ore_lucrate;
+
+            if (salariu > 3000)
+            {
+                Console.WriteLine("Salariu mare");
+            }
+            else
+            {
+                Console.WriteLine("Ati lucrat prea putine ore pentru a avea un salariu mare!.");
+            }
+        }
+    }
+}
