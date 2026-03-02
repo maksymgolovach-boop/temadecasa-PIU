20a78c95c52169676699418bfcc9e64eda1b2d54
maksymgolovach-boop <maksym.holovach@student.usv.ro>
2026-03-02 18:49:20 +0200

    Punctul 1 pentru tema de casa la PIU
    Punctul 1 pentru tema de casa pentru primul laborator la PIU al studentului Holovach Maksym din grupa 3122B

---
+++ Temacasa1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculsalariu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ore_lucrate = 0;
+            double tarif_ora;
+            double salariu;
+
+            Console.WriteLine("Introduceti tariful pe ora [RON]: ");
+            tarif_ora = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduceti numarul de ore lucrate: ");
+            ore_lucrate = Convert.ToInt32(Console.Read());
+
+            salariu = tarif_ora * (double)ore_lucrate;
+
+            if (salariu > 3000)
+            {
+                Console.WriteLine("Salariu mare");
+            }
+            else
+            {
+                Console.WriteLine("Ati lucrat prea puține ore pentru a avea un salariu mare!.");
+            }
+        }
+    }
+}
